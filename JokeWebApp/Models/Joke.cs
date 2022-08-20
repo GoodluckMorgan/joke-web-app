fb142f6318ebad06f73cc6a4f7897fb7b2cbb195
Morgangoodluckcode <mgoodluck97@gmail.com>
2022-08-20 19:23:10 +0100

    Initial commit

---
+++ JokeWebApp/Models/Joke.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public int id  { get; set; }
+
+        public string JokeQuestions { get; set; }
+
+        public string JokeAnswers { get; set; }
+
+
+
+        public Joke()
+        {
+                
+        }
+    }   
+}
+    
